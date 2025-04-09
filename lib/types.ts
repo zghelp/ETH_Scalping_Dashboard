f778a6b604b2f47bf03b4132382d10e09ccb2120
Jason Zhang <xunxuntj@gmail.com>
2025-04-10 02:12:22 +0800

    feat(types): add CandleData interface for candlestick data representation

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export interface CandleData {
+    timestamp: number
+    open: number
+    close: number
+    high: number
+    low: number
+    volume: number
+    EMA5: number
+    EMA20: number
+    RSI: number
+  }
+  
